8c95eb3e4456dc3cf5377f8488527a45fc8573ae
InaQuadt <75311253+InaQuadt@users.noreply.github.com>
2020-12-08 09:13:38 +0000

    Update experiment.js

--- experiment.js
+++ experiment.js
@@ -1,3 +1,10 @@
+  
+/* Change 1: Adding the image hosting site */
+// define the site that hosts stimuli images
+// usually https://<your-github-username>.github.io/<your-experiment-name>/
+var repo_site = "https://inaquadt.github.io/Simon-shape-task/";
+
+
 /* ************************************ */
 /* Define helper functions */
 /* ************************************ */
@@ -104,7 +111,7 @@ var choices = [70, 74]
 var current_trial = 0
 var gap = Math.floor(Math.random() * 2000) + 1000
 var test_stimuli = [{
-  stimulus: '<div class = centerbox><img src="img/orange.png" class="simon_left" /></div>',
+  stimulus: '<div class = centerbox><img src= + repo_site + "img/orange.png" class="simon_left" /></div>',
   data: {
     correct_response: correct_responses[0][1],
     stim_side: 'left',
@@ -113,7 +120,7 @@ var test_stimuli = [{
   },
   key_answer: correct_responses[0][1]
 }, {
-  stimulus: '<div class = centerbox><img src="img/orange.png" class="simon_right" /></div>',
+  stimulus: '<div class = centerbox><img src= + repo_site + "img/orange.png" class="simon_right" /></div>',
   data: {
     correct_response: correct_responses[0][1],
     stim_side: 'right',
@@ -122,7 +129,7 @@ var test_stimuli = [{
   },
   key_answer: correct_responses[0][1]
 }, {
-  stimulus: '<div class = simon_leftbox><img src="img/blue.png" class="simon_left" /></div>',
+  stimulus: '<div class = simon_leftbox><img src= + repo_site + "img/blue.png" class="simon_left" /></div>',
   data: {
     correct_response: correct_responses[1][1],
     stim_side: 'left',
@@ -131,7 +138,7 @@ var test_stimuli = [{
   },
   key_answer: correct_responses[1][1]
 }, {
-  stimulus: '<div class = simon_rightbox><img src="img/blue.png" class="simon_right" /></div>',
+  stimulus: '<div class = simon_rightbox><img src= + repo_site + "img/blue.png" class="simon_right" /></div>',
   data: {
     correct_response: correct_responses[1][1],
     stim_side: 'right',
