bcf405c3f131cd4140006112567d88f2a0f445e8
Jens Quadt <jens.quadt@outlook.com>
2020-12-12 15:16:15 +0100

    Update experiment.js

--- experiment.js
+++ experiment.js
@@ -298,7 +298,7 @@ var test_block = {
 };
 
 var debrief_block = {
-    type: "html-keyboard-response",
+    type: 'poldrack-text',
     stimulus: function () {
 
         var trials = jsPsych.data.get().filter({
@@ -310,9 +310,9 @@ var debrief_block = {
         var accuracy = Math.round(correct_trials.count() / trials.count() * 100);
         var rt = Math.round(correct_trials.select('rt').mean());
 
-        return "<p>You responded correctly on " + accuracy + "% of the trials.</p>" +
+        /*return "<p>You responded correctly on " + accuracy + "% of the trials.</p>" +
             "<p>Your average response time was " + rt + "ms.</p>" +
-            "<p>Press any key to complete the experiment. Thank you!</p>";
+            "<p>Press any key to complete the experiment. Thank you!</p>";*/
 
     }
 };
